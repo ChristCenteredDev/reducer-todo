d2f233cbc03945915aa5f820b0db4ad8bccd54df
William M <wmooney1984@gmail.com>
2019-11-19 21:42:21 -0500

    CRA & Displayed Data

---
+++ todo/src/reducers/reducers.js
@@ -0,0 +1,21 @@
+export const initialState = [
+  {
+    item: 'Learn about reducers',
+    completed: false,
+    id: 8675309
+  },
+  {
+    item: 'Read the Holy Bible',
+    completed: true,
+    id: 8675308
+  },
+  {
+    item: 'Spend time with my wife',
+    completed: false,
+    id: 8675307
+  }
+];
+
+export const reduceTodo = (state, action) => {
+  return state;
+};
