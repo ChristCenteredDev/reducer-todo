eea900133c35767cb122264b4c5869b7f1899a47
William M <wmooney1984@gmail.com>
2019-11-19 22:09:21 -0500

    MVP Completed

--- todo/src/reducers/reducers.js
+++ todo/src/reducers/reducers.js
@@ -1,21 +1,56 @@
-export const initialState = [
-  {
-    item: 'Learn about reducers',
-    completed: false,
-    id: 8675309
-  },
-  {
-    item: 'Read the Holy Bible',
-    completed: true,
-    id: 8675308
-  },
-  {
-    item: 'Spend time with my wife',
-    completed: false,
-    id: 8675307
-  }
-];
+export const initialState = {
+  todos: [
+    {
+      item: 'Learn about reducers',
+      completed: false,
+      id: 8675309
+    },
+    {
+      item: 'Read the Holy Bible',
+      completed: true,
+      id: 8675308
+    },
+    {
+      item: 'Spend time with my wife',
+      completed: false,
+      id: 8675307
+    }
+  ]
+};
 
 export const reduceTodo = (state, action) => {
-  return state;
+  let newTodo = {
+    item: action.payload,
+    completed: false,
+    id: Date.now()
+  };
+  switch (action.type) {
+    case 'ADD_TODO':
+      return {
+        ...state,
+        todos: [...state.todos, newTodo]
+      };
+    case 'TOGGLE_TODO':
+      return {
+        ...state,
+        todos: state.todos.map(todo => {
+          if (action.payload.id === todo.id) {
+            console.log(todo);
+            return {
+              ...todo,
+              completed: !todo.completed
+            };
+          } else {
+            return todo;
+          }
+        })
+      };
+    case 'CLEAR_COMPLETED':
+      return {
+        ...state,
+        todos: state.todos.filter(todo => !todo.completed)
+      };
+    default:
+      return state;
+  }
 };
